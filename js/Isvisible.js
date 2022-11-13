b33544168d0f372c1bf4ea385dbd30c4a47ef23b
lommel <monkeytitle@hotmail.com>
2022-11-13 17:02:22 +0700

    add: animation bush success :tada:

--- js/Isvisible.js
+++ js/Isvisible.js
@@ -1,13 +1,25 @@
-const observable = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('avatar-img');
-           
-            console.log('visible');
-        } else {
-            entry.target.classList.remove('avatar-img');
-            console.log('not visible');
-        }
-    });
+const observable = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("avatar-img");
+
+      console.log("visible");
+    } else {
+      entry.target.classList.remove("avatar-img");
+      console.log("not visible");
+    }
+  });
 });
-observable.observe(document.querySelector('#avatar > img '));
+const bush = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("bush");
+    } else {
+      entry.target.classList.remove("bush");
+      console.log("not visible");
+    }
+  });
+});
+
+observable.observe(document.querySelector("#avatar > img "));
+bush.observe(document.querySelector("#bush"))
